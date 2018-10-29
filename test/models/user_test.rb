ca9df6da0f0c0acb5aa6da21f3c0975f7afcda60
Risa Fujii <risaf1231@gmail.com>
2018-10-29 21:59:47 +0900

    Add test test_user_cannot_have_blank_name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
+# assert: assert that the next statement is true
+# assert_not: assert that the next statement is false
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user cannot have blank name" do
+    user = User.new(name: nil, fashion_choice: 'Halloween')
+    assert_not user.save
+    user.name = ''
+    assert_not user.save
+    user.name = 'Bob'
+    assert user.save
+  end
 end
